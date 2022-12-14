d31231e7e31e659e9cec554b9ae9961c0c17fdc8
NameSecondName1 <name2022secondname2022@mail.ru>
2022-12-14 16:28:31 +0300

    feat: добавить поля классам

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
+import java.time.LocalDate;
+
 public class Film {
+    int id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class User {
     int id;
+    String email;
+    String login;
+    String name;
+    LocalDate birthday;
+
 }
